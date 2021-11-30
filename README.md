f5dc5fb82b720ee12567c7e4b6cc9704d83ab6f8
András Hann <andrashann@users.noreply.github.com>
2021-11-30 23:40:53 +0100

    initial public commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# geomap-add-gpx
-load waypoints and tracks from a GPX file into the GeoMap interface of geocaching.hu via a bookmarklet
+# 🇺🇸 geomap-add-gpx
+
+## Intro 
+
+[geocaching.hu](https://geocaching.hu) is the Hungarian geocaching website with a different (albeit overlapping) set of geocaches than [geocaching.com](https://geocaching.com). 
+
+Their [GeoMap interface](https://geocaching.hu/geomap/) is a handy tool to get a quick map-based overview of geocaches hosted on the website, however, one might want to display a GPX file with its tracks and waypoints in addition to the geocaches that come from the server. This is what this bookmarklet does.
+
+![geocaches and a local GPX file on the same map](geomap-add-gpx.jpg)
+
+*Result of using the bookmarklet: a local GPX file displayed on the map along with the geocaches.*
+
+## Usage
+
+1. Save the following address to your bookmarks bar: `javascript:(function(){document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/andrashann/geomap-add-gpx@main/bookmarklet.min.js';}());`
+1. Go to [GeoMap](https://geocaching.hu/geomap/), wait for everything to load, then click the bookmarklet.
+1. Select the GPX file you want to display.
+1. Type the name of the color that you want to use to display the data.
+1. Enjoy your data on the map.
+
+PS: check out the sister project [geomap-add-lines](https://github.com/andrashann/geomap-add-lines), which connects the points of multi-caches on the map.
+
+# 🇭🇺 geomap-add-lines
+
+## Bevezető 
+
+A [GeoMap](https://geocaching.hu/geomap/) a [geocaching.hu](https://geocaching.hu) térképes felülete. Időnként praktikus lenne a geoládák mellet térképen megjeleníteni egy GPX fájlt is, amiben saját trackek és pontok találhatók. Pontosan ezt teszi lehetővé ez a bookmarklet.
+
+![geoládák és egy GPX file adatai a térképen](geomap-add-gpx.jpg)
+
+*A bookmarklet használatának eredménye: a geoládák mellett a lokális GPX file tartalma is megjelenik a térképen.*
+
+## Használat
+
+1. Mentsd el az alábbi hivatkozást a könyvjelzőid közé: `javascript:(function(){document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/andrashann/geomap-add-gpx@main/bookmarklet.min.js';}());`
+1. Menj a [GeoMapre](https://geocaching.hu/geomap/) és bökj az új könyvjelzőre (bookmarkletre)!
+1. Válaszd ki a megjelenítendő GPX filet!
+2. Írd be annak a színnek a nevét, amit a fájl megjelenítéséhez szeretnél használni.
+3. Élvezd a térképen megjelenített adataidat.
+
+UI: nézd meg a szorosan kapcslódó testvérprojektet is: [geomap-add-lines](https://github.com/andrashann/geomap-add-lines), ami a multiládák pontjait köti össze a térképen.
---
+++ bookmarklet.js
@@ -0,0 +1,99 @@
+// we will parse the xml of the gpx and add its contents to the Leaflet map
+// defined in the Geomap window.
+
+// we need an xml parser, load it if it has not been loaded yet
+if (typeof parser === 'undefined') {
+  document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src = 'https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.21.1/parser.min.js';
+}
+
+// these are the colors that the leaflet-color markers package has 
+// (https://github.com/pointhi/leaflet-color-markers) with the relevant route
+// colors
+const colors = {
+  blue: '#2A81CB',
+  gold: '#FFD326',
+  red: '#CB2B3E',
+  green: '#2AAD27',
+  orange: '#CB8427',
+  yellow: '#CAC428',
+  violet: '#9C2BCB',
+  grey: '#7B7B7B',
+  black: '#3D3D3D',
+}
+
+// set timeout to wait for parser to be loaded
+setTimeout(() => {
+  // open a prompt to select a gpx file
+  const fileSelector = document.createElement('input');
+  fileSelector.setAttribute('type', 'file');
+  fileSelector.setAttribute('accept', '.gpx,.GPX');
+  fileSelector.click();
+
+  fileSelector.addEventListener(
+    'change', // when a file is loaded
+    function () {
+      if (fileSelector.files.length == 1) {
+        const theFile = fileSelector.files[0]
+        // let the user pick a color from the list (this is a string input, so 
+        // ther might be a typo, but there is a default just in case
+        let color = prompt("Válassz színt az alábbiak közül/Select one of these colors: 'blue', 'gold', 'red', 'green', 'orange', 'yellow', 'violet', 'grey', 'black'", "violet");
+        if (!color in colors) { color = 'violet' }
+
+        const reader = new FileReader()
+        reader.readAsText(theFile, 'UTF-8')
+        reader.onload = (e) => {
+          const parsedGPX = parser.parse(e.target.result, {
+            attributeNamePrefix: '',
+            // this is very important as lat and lon won't be loaded otherwise:
+            ignoreAttributes: false,
+            parseNodeValue: true,
+            parseAttributeValue: true,
+            arrayMode: /^trk/,
+          })
+
+          const tracks = parsedGPX.gpx.trk
+          const points = parsedGPX.gpx.wpt
+          console.log(tracks)
+
+          if (points) {
+            // if we have waypoints, add them as it is customary in Leaflet
+            points.forEach((wpt) => {
+              var marker = L.marker([wpt.lat, wpt.lon], {
+                icon: L.icon({
+                  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-' + color + '.png',
+                  iconSize: [25, 41],
+                  iconAnchor: [12, 41],
+                  popupAnchor: [1, -34],
+                })
+              });
+              // we add a popup with the name and description of the waypoint
+              // (opened when clicked)
+              marker.bindPopup('<b>' + wpt.name + ':</b> ' + wpt.desc);
+              marker.addTo(map);
+            })
+          }
+
+          tracks.forEach((trk) => {
+            // add each track -- n.b. the trk and trkseg objects are part of 
+            // the gpx specification
+            trk.trkseg.forEach((trkseg => {
+              const latLons = trkseg.trkpt.map((p) => [p.lat, p.lon])
+              const pL = L.polyline(latLons, { color: colors[color], weight: 8, opacity: 0.4 })
+              // highlight the track on mouseover for better visibility
+              pL.on('mouseover', function (e) {
+                e.target.setStyle({ opacity: 0.8, color: 'white' });
+              });
+              pL.on('mouseout', function (e) {
+                e.target.setStyle({ opacity: 0.4, color: colors[color] });
+              });
+              pL.bindPopup(trk.name + '');
+              pL.addTo(map);
+            }))
+          })
+        }
+      }
+      // destroy the fileSelector element as we don't need it anymore
+      fileSelector.remove()
+    },
+  );
+}, 500);
